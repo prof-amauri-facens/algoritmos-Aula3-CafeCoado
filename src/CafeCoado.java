b82ccf59937c40053fbb7f922899a9ae3770eb84
Amauri Antunes Filho <amauri.antunesfilho@gmail.com>
2024-01-24 21:20:47 -0300

    adicionado algoritmo

--- src/CafeCoado.java
+++ src/CafeCoado.java
@@ -1,5 +1,13 @@
 public class CafeCoado {
     public static void main(String[] args) {
-         System.out.println("Hello world!");
+         System.out.println("Ferver agua");
+         System.out.println("Colocar filtro de papel em suporte");
+         System.out.println("Adicionar café em pó no filtro");
+         System.out.println("Posicionar o suporte sobre a xícara ou recipiente");
+         System.out.println("Deseéjar agua quente sbore o café no filtro");
+         System.out.println("Deixar o café coar lentamente");
+         System.out.println("remover o filtro com o café");
+         System.out.println("Descartar o filtro usado");
+         System.out.println("Servir o café na xicara");
     }
 }
